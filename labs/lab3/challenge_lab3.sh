2e84c43c9d9248f88598f166c5015e812eb6b851
Troppling <hernandezp119@gmail.com>
2024-02-26 03:45:22 -0500

    lab 3 finished

---
+++ labs/lab3/challenge_lab3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Author: Pedro Hernandez
+#Date: current date in format Feb 26th 24
+#Description: This is a basic script that
+#uses environment variables and the date command
+echo "The current date is: "
+date --rfc-email
+echo
+echo "Some information about my system"
+uname --all
+echo
+echo "Ram Information"
+free
+echo
+echo Challege Question Complete
